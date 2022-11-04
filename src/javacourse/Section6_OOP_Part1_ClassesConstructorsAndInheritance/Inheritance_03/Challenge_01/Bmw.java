9d4ad4f38224ab80101189fa96ac7c7cad8df9a1
= <=>
2022-11-05 00:33:08 +0100

    herencias

---
+++ src/javacourse/Section6_OOP_Part1_ClassesConstructorsAndInheritance/Inheritance_03/Challenge_01/Bmw.java
@@ -0,0 +1,18 @@
+package javacourse.Section6_OOP_Part1_ClassesConstructorsAndInheritance.Inheritance_03.Challenge_01;
+
+public class Bmw extends Car{
+
+    private String specificBmwGear;
+
+
+    public Bmw(int speed, String steering, String fuel, String changingGear, int wheels, String specificBmwGear) {
+        super(speed, steering, fuel, changingGear, wheels);
+        this.specificBmwGear = specificBmwGear;
+    }
+
+    // METHOD OVERLOADING
+    public Bmw(int speed, String steering, String fuel, String changingGear, int wheels) {
+        super(speed, steering, fuel, changingGear, wheels);
+        this.specificBmwGear = "No extras specified";
+    }
+}
---
+++ src/javacourse/Section6_OOP_Part1_ClassesConstructorsAndInheritance/Inheritance_03/Challenge_01/Main.java
@@ -0,0 +1,10 @@
+package javacourse.Section6_OOP_Part1_ClassesConstructorsAndInheritance.Inheritance_03.Challenge_01;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Bmw a3 = new Bmw(30, "Straight forward", "Oil", "Break every two years" ,4);
+
+    }
+}
